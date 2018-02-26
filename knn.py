eda138a5f91491987b2697c9cc646afbb0d6f2ec
Felipe de Jesús Salas Casasola <frousterz@gmail.com>
2018-02-25 23:32:43 -0600

    Improve KNN.
     - Create a new method to tabulate results.
    - Improve conditional statements.

--- knn.py
+++ knn.py
@@ -2,15 +2,13 @@
 # Carnet:    2014129277
 # Curso:     Inteligencia Artificial
 # Profesor:  Jose Carranza
-
-# Codepad
-# link Collaboration: https://codepad.remoteinterview.io/QRBULOOYTP
-# Editor Personal: https://codepad.remoteinterview.io/OXARXJQEKI
-
+# Tarea: Implementacion del Algoritmo: K Nearest Neighbors
+# Fecha:     25/02/2018
 
 import numpy as np
-import pickle
+import pickle as pckl
 import heapq as hq
+from tabulate import tabulate
 
 class Image:
   def __init__(self, data, label):
@@ -23,24 +21,60 @@ class Image:
   def get_label(self):
     return self.label
 
-
 class ImageClassifier:
   def __init__(self, training_data, test_data):
     self.training_data = training_data
     self.test_data = test_data
     self.hits = 0
-  
+    
+  def levenshtein(self, k = 1):
+    test = self.test_data
+    training = self.training_data
+    for test_iterator in range(len(test)):
+      results = []
+      test_element = test[test_iterator].data
+      for training_iterator in range(len(training)):
+        training_element = training[training_iterator].data
+        diff = np.where(test_element != training_element)
+        results.append(len(diff[0]))
+
+      min_changes = hq.nsmallest(k, results)
+      for iterator in range(len(min_changes)):
+        min_index = results.index(min_changes[iterator])
+        if(self.training_data[min_index].label == self.test_data[test_iterator].label):
+          self.hits += 1
+          break          
+    tabulate_result(k, self.hits, ((self.hits*100))/len(test))
+
   def manhattan(self, k = 1):
     test = self.test_data
     training = self.training_data
-    #len(test)
-    for test_iterator in range(10):
+    for test_iterator in range(len(test)):
       results = []
       test_element = test[test_iterator].data
 
       for training_iterator in range(len(training)):
         training_element = training[training_iterator].data
-        results.append(np.sum(abs(test_element - training_element)))
+        results.append(np.sum(np.abs(test_element - training_element)))
+
+      min_distances = hq.nsmallest(k, results)        
+      for iterator in range(len(min_distances)):
+        min_index = results.index(min_distances[iterator])
+        if(self.training_data[min_index].label == self.test_data[test_iterator].label):
+          self.hits += 1
+          break
+    tabulate_result(k, self.hits, ((self.hits*100))/len(test))
+
+  def chebyshev(self, k = 1):
+    test = self.test_data
+    training = self.training_data
+    for test_iterator in range(len(test)):
+      results = []
+      test_element = test[test_iterator].data
+
+      for training_iterator in range(len(training)):
+        training_element = training[training_iterator].data
+        results.append(np.amax(np.absolute(test_element - training_element)))
 
       min_distances = hq.nsmallest(k, results)
       for iterator in range(len(min_distances)):
@@ -48,90 +82,9 @@ class ImageClassifier:
         if(self.training_data[min_index].label == self.test_data[test_iterator].label):
           self.hits += 1
           break
-            
-      print("Iteracion ->" + str(test_iterator))
-          
-    print("k = " + str(k)
-          + " :: aciertos: " + str(self.hits)
-          + " :: % exito: " + str((self.hits*100)/10))
+    tabulate_result(k, self.hits, ((self.hits*100))/len(test))
 
-  def chebyshev(self, k = 1):
-    test = self.test_data
-    training = self.training_data
-    for test_iterator in range(10):
-      results = []
-      test_element = test[test_iterator].data
 
-      for training_iterator in range(len(training)):
-        training_element = training[training_iterator].data
-        results.append(max(np.absolute(test_element - training_element)))
-
-      if(k == 1):
-        max_index = np.argmax(results)
-        if(self.training_data[max_index].label == self.test_data[test_iterator].label):
-          self.hits += 1
-
-      if(k == 2):
-        max_distances = hq.nlargest(2, results)
-        for iterator in range(len(max_distances)):
-          max_index = results.index(max_distances[iterator])
-          if(self.training_data[max_index].label == self.test_data[test_iterator].label):
-            self.hits += 1
-
-      if(k == 3):
-        max_distances = hq.nlargest(3, results)
-        for iterator in range(len(max_distances)):
-          max_index = results.index(max_distances[iterator])
-          if(self.training_data[max_index].label == self.test_data[test_iterator].label):
-            self.hits += 1
-            
-      print("Iteracion ->" + str(test_iterator))
-          
-    print("k = " + str(k)
-          + " :: aciertos: " + str(self.hits)
-          + " :: % exito: " + str((self.hits*100)/10))
-    
-  def levenshtein(self, k = 1):
-    test = self.test_data
-    training = self.training_data
-    # len(test)
-    for test_iterator in range(100):
-      results = []
-      test_element = test[test_iterator].data
-
-      for training_iterator in range(len(training)):
-        training_element = training[training_iterator].data
-        diff = np.where(test_element != training_element)
-        results.append(len(diff[0]))
-
-      if(k == 1):
-        min_index = np.argmin(results)
-        if(self.training_data[min_index].label == self.test_data[test_iterator].label):
-          self.hits += 1
-
-      if(k == 2):
-        min_changes = hq.nsmallest(2, results)
-        for iterator in range(len(min_changes)):
-          min_index = results.index(min_changes[iterator])
-          if(self.training_data[min_index].label == self.test_data[test_iterator].label):
-            self.hits += 1
-            break
-
-      if(k == 3):
-        min_changes = hq.nsmallest(3, results)
-        for iterator in range(len(min_changes)):
-          min_index = results.index(min_changes[iterator])
-          if(self.training_data[min_index].label == self.test_data[test_iterator].label):
-            self.hits += 1
-            break
-      
-      print("Iteracion ->" + str(test_iterator))
-          
-    print("k = " + str(k)
-          + " :: aciertos: " + str(self.hits)
-          + " :: % exito: " + str((self.hits*100)/100))
-
-    
 class DataManager:
   def __init__(self):
     self.training_data = []
@@ -145,10 +98,10 @@ class DataManager:
 
   def load_training_data(self):
     print("Loading training data...")
-    for n in range(1,2):
+    for n in range(2,3):
       print("File Loaded!")
       with open("test/data_batch_" + str(n), 'rb') as file_path:
-        file_loaded = pickle.load(file_path, encoding='bytes')
+        file_loaded = pckl.load(file_path, encoding='bytes')
         data_loaded = file_loaded[b'data']
         labels_loaded = file_loaded[b'labels']
         for it in range (len(data_loaded)):
@@ -158,7 +111,7 @@ class DataManager:
   def load_test_data(self):
     print("Loading test data...")
     with open("test/test_batch", 'rb') as file_path:
-      file_loaded = pickle.load(file_path, encoding='bytes')
+      file_loaded = pckl.load(file_path, encoding='bytes')
       data_loaded = file_loaded[b'data']
       labels_loaded = file_loaded[b'labels']
       for it in range (len(data_loaded)):
@@ -166,25 +119,26 @@ class DataManager:
     print("Test data loaded successfully!")
 
 
-"""
-  The main function is used to create and call objects and actions/methods
-  to test all the functionality.
-"""
-
+# Global Methods
 def main():
-  print("Hello KNN")
+  print("K Nearest Neighbors")
+  dm = DataManager()
+  dm.load_training_data()
+  dm.load_test_data()
+  classifier = ImageClassifier(dm.training_data, dm.test_data)
+  classifier.manhattan() # Al usar el metodo asi, k por defecto es #1
+  """
+    classifier.manhattan(2) usa k = 2, y asi con el valor de k deseado.
+  """
 
-dm = DataManager()
-dm.load_training_data()
-dm.load_test_data()
-i = ImageClassifier(dm.training_data, dm.test_data)
-
-"""
-Para probar todo solo use:
->>> i.distance_method()
-ahi escoge el que quiera.
-por defecto k =1 asi que no es necesario que le ponga parametro, a no ser de que use k = 2 o k=3
-
-Si quiere probar otro vuelva a reiniciar el estos pasos pero con otro metodo de distancia.
-"""
-
+def tabulate_result(k, hits, success_percentage):
+  print(
+    tabulate(
+      [
+        ["k",k],
+        ["Hits", hits],
+        ["Success", str(success_percentage)+ "%"]
+      ],
+      headers = ['Variable Name', 'Value'
+    ])
+  )
